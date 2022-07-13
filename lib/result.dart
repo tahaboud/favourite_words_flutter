737a04b02612bf8c67bbfd16a31d33bcb124614d
Boudouaoui Taha <tahaboudouaoui@gmail.com>
2022-07-13 20:21:07 +0100

    finish first app

---
+++ lib/result.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = "You are awesome and innocent!";
+    } else if (resultScore <= 12) {
+      resultText = "Pretty likeable!";
+    } else if (resultScore <= 16) {
+      resultText = "Your are strange!";
+    } else {
+      resultText = "You are so bad!";
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Center(
+          child: Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        FlatButton(onPressed: resetHandler, child: Text("Restart Quizz!"))
+      ],
+    );
+  }
+}
